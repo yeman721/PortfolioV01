8cd5468f32269b33fd7b4b22030c2c725de5b04b
Gary Ye <yeman721@gmail.com>
2018-06-24 14:22:43 -0700

    Header and footer update, color update

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function scrollUpdate () {
     // Disable scroll-jumping for a short time to avoid flickering
     window.setTimeout(function () {
       disableScroll = false;
-    }, 40);
+    }, 100);
   }
 }
 
